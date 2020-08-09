3347c46c4e09afa248184df8ff4f873d45a65ece
Dhayana <contatodhayana@gmail.com>
2020-08-09 01:32:14 -0300

    aula 05

--- mobile/src/routes/StudyTabs.tsx
+++ mobile/src/routes/StudyTabs.tsx
@@ -51,7 +51,7 @@ function StudyTabs() {
                 name="Favorites" 
                 component={Favorites}
                 options={{
-                    tabBarLabel: 'Proffys',
+                    tabBarLabel: 'Favoritos',
                     tabBarIcon: () => {
                         return (
                             <Ionicons name="ios-heart" />
